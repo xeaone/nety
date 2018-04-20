a8b2d9aa3a9ddb3c94986d6585f784ff13569722
Alexander Elias <alex.steven.elias@gmail.com>
2018-04-20 10:21:12 -0700

    replaced try/catch

--- test/index.js
+++ test/index.js
@@ -1,38 +1,39 @@
+
 const Servey = require('../index');
 const Path = require('path');
 
-(async function () { try {
+(async function () {
 
-		const options = {
-			spa: true,
-			cors: true,
-			cache: true,
-			port: 8080,
-			folder: Path.join(__dirname, 'static')
-		};
+	const server = Servey.create({
+		spa: true,
+		cors: true,
+		cache: true,
+		port: 8080,
+		folder: Path.join(__dirname, 'static')
+	});
 
-		const server = Servey.create(options);
+	server.on('error', function (error) {
+		console.error(error);
+	});
 
-		server.on('error', function (error) {
-			console.error(error);
-		});
+	server.on('request', function (req) {
+		console.log(req.url);
+	});
 
-		server.on('request', function (req) {
-			console.log(req.url);
-		});
+	server.on('open', function () {
+		console.log('open');
+	});
 
-		server.on('open', function () {
-			console.log('open');
-		});
+	await server.open();
 
-		await server.open();
+	console.log('server open');
+	console.log(server.port);
 
-		console.log('server open');
-		console.log(server.port);
+	// setTimeout(async function () {
+	// 	await server.close();
+	// 	console.log('server close');
+	// }, 3000);
 
-		// setTimeout(async function () {
-		// 	await server.close();
-		// 	console.log('server close');
-		// }, 3000);
-
-} catch (e) { console.log(e); } }());
+}()).catch(function (error) {
+	console.error(error);
+});
