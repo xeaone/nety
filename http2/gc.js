c4ad2b46acb1819be824e23c87a938f039a435ab
Alexander Elias <alex.steven.elias@gmail.com>
2019-05-03 16:18:59 -0700

    formatting

--- http2/gc.js
+++ http2/gc.js
@@ -4,16 +4,16 @@ const { promisify } = require('util');
 const { exec } = require('child_process');
 const Exec = promisify(exec);
 
-(async function(option) {
-	option = option || {};
+(async function (option) {
+    option = option || {};
 
-	option.cn = 'localhost';
+    option.cn = 'localhost';
 
-	const command = `openssl req -x509 -newkey rsa:2048 -nodes -sha256 ` +
+    const command = 'openssl req -x509 -newkey rsa:2048 -nodes -sha256 ' +
 		`-subj '/CN=${option.cn}' -keyout ${option.cn}-privkey.pem -out ${option.cn}-cert.pem`;
 
-	const result = await Exec(command);
+    const result = await Exec(command);
 
-	console.log(result);
+    console.log(result);
 
 }()).catch(console.error);
