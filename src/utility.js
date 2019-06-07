f0579681f7b18c6148b38f0aa6c3b49d7654071a
Alexander Elias <alex.steven.elias@gmail.com>
2019-06-07 11:35:08 -0700

    added comment and compare methods

--- src/utility.js
+++ src/utility.js
@@ -17,12 +17,6 @@ module.exports = {
         });
     },
 
-    methodNormalize (method) {
-        return typeof method === 'string' ? method.toUpperCase() : method.map(function (item) {
-            return item.toUpperCase();
-        });
-    },
-
     toCamelCase (data) {
         data = data.slice(0, 1).toLowerCase() + data.slice(1);
         return data.replace(/(_+|-+|\s+)([a-zA-z])/g, function (string) {
@@ -30,6 +24,72 @@ module.exports = {
         });
     },
 
+    compareMethod (routeMethod, userMethod) {
+
+        if (typeof routeMethod === 'string') {
+            return routeMethod.toUpperCase() === userMethod.toUpperCase();
+        } else {
+            for (const method of routeMethod) {
+                if (this.compareMethod(method, userMethod)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    },
+
+    comparePath (routePath, userPath) {
+
+        if (typeof routePath === 'string') {
+            const compareParts = [];
+            const userParts = userPath.replace(/^\/|\/$/g, '').split(/\/|-/);
+            const routeParts = routePath.replace(/^\/|\/$/g, '').split(/\/|-/);
+
+            for (let i = 0, l = routeParts.length; i < l; i++) {
+
+                if (routeParts[i].startsWith('(') && routeParts[i].endsWith(')')) {
+
+                    if (routeParts[i] === '(~)' || routeParts[i] === '(*)') {
+                        return true;
+                    } else {
+                        compareParts.push(userParts[i]);
+                    }
+
+                } else if (routeParts[i] !== userParts[i]) {
+                    return false;
+                } else {
+                    compareParts.push(routeParts[i]);
+                }
+
+            }
+
+            if (compareParts.join('/') === userParts.join('/')) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            for (const path of routePath) {
+                if (this.comparePath(path, userPath)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    },
+
+    async getMime (data) {
+        data = data || '';
+
+        if (data.includes('.')){
+            data = Path.extname(data).slice(1);
+        }
+
+        return this.mimes[data || 'txt'];
+    }
     // assign (target, options, defaults) {
     //
     // 	for (let name in defaults) {
@@ -47,14 +107,30 @@ module.exports = {
     //     return target;
     // },
 
-    async getMime (data) {
-        data = data || '';
-
-        if (data.includes('.')){
-            data = Path.extname(data).slice(1);
-        }
-
-        return this.mimes[data || 'txt'];
-    }
+    // toParameterObject (routePath, userPath) {
+    //     let result = {};
+    //
+    //     if (
+    //         !routePath
+	// 		|| !userPath
+	// 		|| routePath === '/'
+	// 		|| userPath === '/'
+    //     ) return result;
+    //
+    //     const userParts = userPath.split(/\/|-/);
+    //     const routeParts = routePath.split(/\/|-/);
+    //
+    //     for (let i = 0, l = routeParts.length; i < l; i++) {
+    //         let part = routeParts[i];
+    //
+    //         if (part.slice(0, 1) === '(' && part.slice(-1) === ')') {
+    //             const name = part.slice(1, part.length - 1).replace('~', '');
+    //             result[name] = userParts[i];
+    //         }
+    //
+    //     }
+    //
+    //     return result;
+    // }
 
 };
